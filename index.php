4491df7841d5ef22e96f31b3bb992b7c60105251
Tommie <tommie.rundberg@gmail.com>
2022-02-07 21:41:17 +0100

    update

--- index.php
+++ index.php
@@ -18,6 +18,8 @@ header('Content-type: text/html; charset=latin1_swedish_ci');
 @import url('https://font.googleapis.com/css2?family=Open+Sans:wght@300&dispay=swap');
 body{
 font-family: 'Open Sans',sans-serif;
+margin:0px;
+padding:0px;
 }
 .header{
   height:50px;
