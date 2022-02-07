dcaa96921f1bf041dd941e03b4299980e8a3d91f
Tommie <tommie.rundberg@gmail.com>
2022-02-07 17:44:02 +0100

    upadte

--- index.php
+++ index.php
@@ -29,11 +29,11 @@ font-family: 'Open Sans',sans-serif;
 
 <?php 
 
-
+/*
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+*/
 
 include 'db_connection.php';
 
