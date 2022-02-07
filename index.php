6f77efe647b55dd7ee568bcede6da5c2570cf523
Tommie <tommie.rundberg@gmail.com>
2022-02-07 21:48:28 +0100

    update

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ header('Content-type: text/html; charset=latin1_swedish_ci');
 <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="apple-mobile-web-app-status-bar-style" content="default">
-<meta http-equiv="refresh" content="10">
+<meta http-equiv="refresh" content="5; url=index.php">
 
 
 <title>Fuktstatus</title>
