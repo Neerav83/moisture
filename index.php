d388fb631dfa20dd4479541b37e5ac17478eed47
Tommie <tommie.rundberg@gmail.com>
2022-02-07 15:35:01 +0100

    update

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+
+
+<html lang="SE" dir="ltr" data-locale="SE" class="">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="default">
+<meta http-equiv="refresh" content="5; url=index.php">
+
+
+<title>Fuktstatus</title>
+<style>
+@import url('https://font.googleapis.com/css2?family=Open+Sans:wght@300&dispay=swap');
+body{
+font-family: 'Open Sans',sans-serif;
+}
+
+</style>
+
+</head>
+
+
+
 <?php 
 
 
@@ -56,26 +80,6 @@ CloseCon($conn);
 ?>
 
 
-
-<html lang="SE" dir="ltr" data-locale="SE" class="">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
-<meta name="apple-mobile-web-app-capable" content="yes" />
-<meta name="apple-mobile-web-app-status-bar-style" content="default">
-<meta http-equiv="refresh" content="5; url=index.php">
-
-
-<title>Fuktstatus</title>
-<style>
-@import url('https://font.googleapis.com/css2?family=Open+Sans:wght@300&dispay=swap');
-body{
-font-family: 'Open Sans',sans-serif;
-}
-
-</style>
-
-</head>
 <br>
 <center><h4>Fuktstatus</h4></center>
 <?php
